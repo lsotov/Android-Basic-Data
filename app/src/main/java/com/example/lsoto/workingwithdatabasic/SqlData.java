fd052cb8ba3934ae705282de03e4ae7f30677d26
lsoto <lsoto27@gmail.com>
2016-11-11 09:53:49 -0600

    Added sql methods

--- app/src/main/java/com/example/lsoto/workingwithdatabasic/SqlData.java
+++ app/src/main/java/com/example/lsoto/workingwithdatabasic/SqlData.java
@@ -1,7 +1,10 @@
 package com.example.lsoto.workingwithdatabasic;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 // Saving data to a database is ideal for repeating or structured data, such as contact information.
@@ -11,7 +14,7 @@ import android.os.Bundle;
 // which explicitly specifies the layout of your schema in a systematic and self-documenting way.
 // A contract class is a container for constants that define names for URIs, tables, and columns.
 public class SqlData extends Activity {
-
+    FeedReaderDbHelper mDbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,87 @@ public class SqlData extends Activity {
 
     public void AccessDb(){
         Context context = getApplicationContext();
-        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(context);
+        mDbHelper = new FeedReaderDbHelper(context);
+    }
+
+    public void WriteToDb(){
+        AccessDb();
+        // Gets the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, "title");
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE, "subtitle");
+
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values);
+    }
+
+    public void ReadFromDb(){
+        AccessDb();
+
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                FeedReaderContract.FeedEntry._ID,
+                FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE,
+                FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE
+        };
+
+        // Filter results WHERE "title" = 'My Title'
+        String selection = FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE + " = ?";
+        String[] selectionArgs = { "My Title" };
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE + " DESC";
+
+        Cursor c = db.query(
+                FeedReaderContract.FeedEntry.TABLE_NAME,  // The table to query
+                projection,                               // The columns to return
+                selection,                                // The columns for the WHERE clause
+                selectionArgs,                            // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+        c.moveToFirst();
+        long itemId = c.getLong(
+                c.getColumnIndexOrThrow(FeedReaderContract.FeedEntry._ID)
+        );
+    }
+
+    public void DeleteFromDb(){
+        AccessDb();
+        // Gets the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        // Define 'where' part of query.
+        String selection = FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE + " LIKE ?";
+        // Specify arguments in placeholder order.
+        String[] selectionArgs = { "MyTitle" };
+        // Issue SQL statement.
+        db.delete(FeedReaderContract.FeedEntry.TABLE_NAME, selection, selectionArgs);
+    }
+
+    public void UpdateToDb(){
+        AccessDb();
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // New value for one column
+        ContentValues values = new ContentValues();
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, "title2");
+
+        // Which row to update, based on the title
+        String selection = FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE + " LIKE ?";
+        String[] selectionArgs = { "MyTitle" };
+
+        int count = db.update(
+                FeedReaderContract.FeedEntry.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs);
     }
 }
